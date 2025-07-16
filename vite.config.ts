d803e9ec36a70128f50d7cbbd562c17f1117a234
prisherious <maximilianschuehrer@gmail.com>
2025-07-16 12:51:09 +0200

    Exclude fsevents from Vite Rollup build for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['fsevents'],
+      external: ['fsevents'], // ❗ Das ignoriert das Modul
     },
   },
 })
