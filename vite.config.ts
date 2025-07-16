aa9cbacf8a137cd27a8433c522ddda3925fd3292
prisherious <maximilianschuehrer@gmail.com>
2025-07-16 13:07:03 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.node'],
   build: {
     rollupOptions: {
-      external: ['fsevents', '../pkg'], // ⬅️ hier explizit ignorieren
+      external: ['fsevents', '../pkg'],
+    },
+    commonjsOptions: {
+      ignoreTryCatch: (id) => true,
     },
   },
-  assetsInclude: ['**/*.node'],
 })
