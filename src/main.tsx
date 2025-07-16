872b42f6085437ed5c5a5e4c73321846e0f5612b
prisherious <maximilianschuehrer@gmail.com>
2025-07-16 14:31:08 +0200

    added app desaign

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.tsx'
 import DynamicPage from './DynamicPage.tsx'
+import './App.css' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
