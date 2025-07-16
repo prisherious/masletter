abbf7c7b33f42ad79f53228b1e40fc981889fca4
prisherious <maximilianschuehrer@gmail.com>
2025-07-16 12:57:36 +0200

    added ..pkg enterfernen

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+  root: './',
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['fsevents'], // ❗ Das ignoriert das Modul
+      external: ['fsevents'],
     },
   },
 })
