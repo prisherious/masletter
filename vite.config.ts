33a56e5b99566fafa5567d7f37271b7aadf74398
prisherious <maximilianschuehrer@gmail.com>
2025-07-16 13:02:04 +0200

    Fix: exclude .node files from import analysis

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  root: './',
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['fsevents','../pkg'],
+      external: ['fsevents'],
     },
   },
+  assetsInclude: ['**/*.node'], // ⬅️ Wichtig!
 })
